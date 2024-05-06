24b865a0061d7d8c4b59201a6e9535d1b53ed4e7
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2024-05-06 15:01:11 +0200

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+sources = import ./nix/sources.nix;
+pkgs = import sources.nixpkgs { };
+bashcards = pkgs.callPackage ./derivation.nix { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    pandoc
+    niv
+  ];
+}
